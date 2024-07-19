865597def82cb53ab8db0383c35d93c4875db32b
Desmondezo1 <desezo@gmail.com>
2024-07-19 15:09:10 +0100

    Added controllers and Requests for Post and Users

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
 
 Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+    return User::all();
+});
