8ea3514a1c32f6c3b889375219a0b121c69421e6
Desmondezo1 <desezo@gmail.com>
2024-07-19 13:52:25 +0100

    removed redundant commented code

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -28,13 +28,6 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            // 'name' => fake()->name(),
-            // 'email' => fake()->unique()->safeEmail(),
-            // 'role' => '',
-            // 'email_verified_at' => now(),
-            // 'password' => static::$password ??= Hash::make('password'),
-            // 'is_active' => true,
-            // 'last_login_at' => now(),
 
             'name' => $this->faker->name,
             'email' => $this->faker->unique()->safeEmail,
